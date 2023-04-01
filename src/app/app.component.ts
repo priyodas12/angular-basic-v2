51558c8965245d29dd42e0e49ecb301af6e168eb
Priyobrato.Das <priyodas12@gmail.com>
2023-04-01 23:59:10 +0530

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-basic-v2';
+  imgUrl = 'https://picsum.photos/id/63/5000/2813';
+  widthPic = '100px';
+  heightPic = '120px';
 }

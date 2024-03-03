61839875aed72f51806919fe0e50c0e021befc79
Priyobrato.Das <priyodas12@gmail.com>
2024-03-03 12:58:46 +0530

    feat: ng-if directive example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-basic-v2';
-  imgUrl = 'https://picsum.photos/id/63/5000/2813';
 
-  changeImage(e: KeyboardEvent) {
-    this.imgUrl = (e.target as HTMLInputElement).value;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
+import { IfDirectiveComponent } from './directives/if-directive/if-directive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    IfDirectiveComponent
   ],
   imports: [
     BrowserModule,
